ff79fb67cd9b46ffad5e520ebff1fe4d987dd702
Isac Fadoni <isacfadoni@gmail.com>
2022-01-03 15:06:01 -0300

    fix(greetings): import type VFC

--- src/modules/ui/views/components/greetings/greetings.tsx
+++ src/modules/ui/views/components/greetings/greetings.tsx
@@ -1,5 +1,6 @@
+import type { VFC } from "react";
 import React from "react";
 
-export const Greetings: React.VFC = () => (
+export const Greetings: VFC = () => (
   <h1 className="text-2xl text-violet-500">Howdy! 🌃</h1>
 );
