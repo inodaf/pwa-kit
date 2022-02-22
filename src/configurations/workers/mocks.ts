32d9f29f3c4b74df767de6102ba44dde3a9e5f12
Isac Fadoni <isacfadoni@gmail.com>
2022-02-22 20:25:19 -0300

    fix(mocks): lint issue with require

--- src/configurations/workers/mocks.ts
+++ src/configurations/workers/mocks.ts
@@ -2,7 +2,6 @@ import { isBrowser } from "@/utilities";
 
 if (process.env.NODE_ENV === "development") {
   if (isBrowser) {
-    const { worker } = require("@config/mocks/browser");
-    worker.start();
+    import("@config/mocks/browser").then((module) => module.worker.start());
   }
 }
