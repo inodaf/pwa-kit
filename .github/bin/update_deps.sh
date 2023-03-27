a4e39e5a6aff20656f93c315844bec4b50b64e78
Isac Fadoni <isac.fadoni@n26.com>
2023-03-27 23:29:03 +0200

    chore(ci): update github_env to correctly set env

--- .github/bin/update_deps.sh
+++ .github/bin/update_deps.sh
@@ -4,7 +4,7 @@
 # and commit the changes.
 
 export BRANCH_NAME="update-dev-deps-`date +%s`"
-echo "::set-env name=BRANCH_NAME::$BRANCH_NAME"
+echo "BRANCH_NAME=$BRANCH_NAME" >> $GITHUB_ENV
 
 setup() {
   if [[ -z $GITHUB_ACTIONS ]]; then exit; fi;
