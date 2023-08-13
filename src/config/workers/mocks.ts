f72d2b7aa4fbbd745a62c24eb3b79cb2a14ccc87
Isac Fadoni <isac.fadoni@n26.com>
2023-08-13 11:19:57 +0200

    Fix formatting issue

--- src/config/workers/mocks.ts
+++ src/config/workers/mocks.ts
@@ -3,7 +3,7 @@ import { isBrowser } from "@/utilities";
 if (process.env.NODE_ENV === "development") {
   if (isBrowser) {
     import("@/tests/config/mocks/browser").then((module) =>
-      module.worker.start({ quiet: true })
+      module.worker.start({ quiet: true }),
     );
   }
 }
